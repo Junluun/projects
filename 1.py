be1904cf46a9f7cd42b60565802de7f6be42975e
Junluun <108047929+Junluun@users.noreply.github.com>
2023-07-25 08:32:58 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -23,6 +23,32 @@ if form.is_valid():
 
 
 
+
+if form.is_valid():
+    instance = form.save(commit=False)
+    fs = FileSystemStorage()
+    for e in form:
+        instance_file = e.instance.file
+        file_path = None
+        file = e.cleaned_data['file']
+        if e.cleaned_data['DELETE'] == False:
+            if file:
+                if any(x in e.changed_data for x in ['file']):
+                    if instance_file:
+                        if fs.exists(instance_file.path):
+                            fs.delete(instance_file.path)
+                    file_path = fs.save(fs.get_available_name(file.name), file)
+            else:
+                file_path = instance_file.name
+        else:
+            if instance_file:
+                if fs.exists(instance_file.path):
+                    fs.delete(instance_file.path)
+                instance_file.close()
+    instance.save()
+
+
+
 if form.is_valid():
     instance = form.save(commit=False)
     del_exp = [p.exp_id for p in form.deleted_objects]
